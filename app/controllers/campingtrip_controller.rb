842e28e3d08a0d66b63d8d31e320b015ef679349
Erica Plofsky <ericaplofsky@gmail.com>
2017-10-11 01:00:01 +0000

    updated create_new trip form

--- app/controllers/campingtrip_controller.rb
+++ app/controllers/campingtrip_controller.rb
@@ -1,2 +1,12 @@
 class CampingtripController < ApplicationController
+
+  get '/campingtrip/new' do
+    if session[:user_id]
+      erb :'/campingtrip/create_trip'
+    else
+      redirect to '/login'
+    end
+  end
+
+
 end

81cd2e725b9ae3557eb5535e52bdef5477a03402
Erica Plofsky <ericaplofsky@gmail.com>
2017-10-14 14:39:38 +0000

    updated campingtrip controller to load campingtrips tble and save

--- app/controllers/campingtrip_controller.rb
+++ app/controllers/campingtrip_controller.rb
@@ -7,15 +7,18 @@ class CampingtripController < ApplicationController
       redirect to '/login'
     end
   end
-  
-  # post '/campingtrip/create_trip' do
-  #     @campingtrip = CampingTrip.new(params)
-  #     if @campingtrip.save && !user_campingtrip.include?(@campingtrip.name)
-  #       current_user.campingtrip << @campingtrip
-  #       flash[:message] = "Successfully created trip"
-  #       redirect to "/campingtrip/#{@campingtrip.id}"
-  #     else
-  #       redirect to '/campingtrip/create_trip'
-  #     end
-  #   end
+
+post '/campingtrip/create_trip' do
+ @campingtrip = Campingtrip.create(park_name: params[:park_name], state: params[:state], site_number: params[:site_number],
+ firepit: params[:firepit], waterfront: params[:waterfront], shaded: params[:shaded], dog_friendly: params[:dog_friendly],
+ cabins: [params[:cabins], toilet_type: params[:toilet_type], other_good_sites: params[:other_good_sites],
+ waterpump: params[:waterpump], water_taste: params[:water_taste], added_notes: params[:added_notes]])
+ if @campingtrip.save && !current_user.campingtrips.include?(@campingtrip.park_name)
+        current_user.campingtrips << @campingtrip
+        flash[:message] = "Successfully created trip"
+        redirect to "/campingtrip/#{@campingtrip.id}"
+      else
+        redirect to '/campingtrip/create_trip'
+      end
+    end
 end
--- app/models/campingtrip.rb
+++ app/models/campingtrip.rb
@@ -1,3 +1,3 @@
-class CampingTrip < ActiveRecord::Base
-  belongs_to :user 
+class Campingtrip < ActiveRecord::Base
+  belongs_to :user
 end
